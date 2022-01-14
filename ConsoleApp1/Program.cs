c05182baecbb556de5434352a891a400c898ac82
sergeyzel <zelionniysergey@yandex.ru>
2022-01-14 12:37:34 +0700

    TestProgramma

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter1.Task1._1.Program.run();
+        }
+    }
+}
